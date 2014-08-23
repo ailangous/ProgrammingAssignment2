dd5046893a6ff1cf251e590821058dd64091e5c1
ailangous <anand.ilango@gmail.com>
2014-08-23 15:55:39 -0500

    Add function code
    Added code to cache solve a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_x <- NULL
+  set <- function(y) {
+    x <<- y
+    inv_x <<- NULL
+  }
+  get <- function() x
+  setinv <- function(invx) inv_x <<- invx
+  getinv <- function() inv_x
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
@@ -12,4 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invx <- x$getinv()
+  if(!is.null(invx)) {
+    message("getting cached data")
+    return(invx)
+  }
+  data <- x$get()
+  invx <- solve(data)
+  x$setinv(invx)
+  invx  
 }
+
